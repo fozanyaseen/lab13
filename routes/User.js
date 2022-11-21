046d1a2abe14d8ba7735b1853ab40620abd950e2
fozanyaseen <114652942+fozanyaseen@users.noreply.github.com>
2022-11-21 14:14:19 +0500

    Update User.js

--- routes/User.js
+++ routes/User.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-let User = require('../models/User');
+let User = require('../Models/User');
 
 router.route('/add').post((req, res) => {
     const Name = req.body.Name;
